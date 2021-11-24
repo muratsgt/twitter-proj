3216621bda94f27a2322294029705b989dcde4bf
Murat <muratakca.mt@gmail.com>
2021-11-24 18:27:41 +0100

    tweet modal created

--- layout/Sidebar.js
+++ layout/Sidebar.js
@@ -1,17 +1,29 @@
+import { useState } from 'react';
 import styles from "./Sidebar.module.css";
 import Navigation from "../components/Navigation";
 import ThemeButton from "../components/buttons/ThemeButton";
 import ProfileButton from "../components/buttons/Profilebutton";
 import { Tweet } from "../components/Icons";
+import 'react-responsive-modal/styles.css';
+import { TweetModal } from '../modals/TweetModal';
+
 
 function Sidebar({ wide = true, children, ...props }) {
+    const [open, setOpen] = useState(false);
+    const onOpenModal = () => setOpen(true);
+    const onCloseModal = () => setOpen(false);
+
     return (
         <div className={styles.sidebar} {...props}>
             <Navigation wide={wide}></Navigation>
-            <ThemeButton full={wide} className={styles.tweet}>
+            <ThemeButton onClick={onOpenModal} full={wide} className={styles.tweet}>
                 {wide ? "Tweet" : <Tweet />}
             </ThemeButton>
             <ProfileButton wide={wide} className={styles.profile} ></ProfileButton>
+            <TweetModal
+                open={open}
+                onCloseModal={onCloseModal}
+            ></TweetModal>
             {children}
         </div>
     )
---
+++ modals/TweetModal/index.js
@@ -0,0 +1,44 @@
+import {useRef} from "react";
+import styles from "./style.module.css";
+import ThemeButton from "../../components/buttons/ThemeButton";
+import { Modal } from 'react-responsive-modal';
+import Avatar from '../../components/Avatar';
+import IconButton from '../../components/buttons/IconButton';
+import * as Icon from "../../components/Icons";
+
+export function TweetModal(props) {
+    const hiddenFileInput = useRef(null);
+    const handleClick = event => {
+        hiddenFileInput.current.click();
+    };
+    const handleChange = event => {
+        const fileUploaded = event.target.files[0];
+        // props.handleFile(fileUploaded);
+    };
+
+    return (
+        <Modal
+            classNames={{ modal: styles.modal }}
+            open={props.open}
+            onClose={props.onCloseModal}
+            center
+        >
+            <div className={styles.modalcontainer}>
+                <Avatar></Avatar>
+                <div className={styles.modalbody}>
+                    <div>
+                        <textarea placeholder="What's happening?" className={styles.inputBox} type="text" />
+                        <input type="file" ref={hiddenFileInput} onChange={handleChange} style={{
+                            display: 'none'
+                        }} />
+                    </div>
+                    <div className={styles.modalbuttons}>
+                        <IconButton onClick={handleClick}><Icon.Media></Icon.Media></IconButton>
+                        <IconButton><Icon.Gif></Icon.Gif></IconButton>
+                        <IconButton><Icon.Emoji></Icon.Emoji></IconButton>
+                        <ThemeButton className={styles.tweetbutton}>Tweet</ThemeButton>
+                    </div>
+                </div>
+            </div>
+        </Modal>);
+}
