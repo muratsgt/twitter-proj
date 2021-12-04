1f1227d92281eb3af2c04194dfc09f23beeeca31
Murat <muratakca.mt@gmail.com>
2021-12-04 01:29:12 +0100

    fix2

--- pages/explore.js
+++ pages/explore.js
@@ -4,8 +4,23 @@ import TrendsBox from "../components/TrendsBox";
 import { useTrends } from "../helper/useFetcher";
 
 // page for explore
-function Explore({  }) {
+function Explore({ }) {
     const { data } = useTrends();
+
+    let { NODE_ENV, DEV_URL, PROD_URL, VERCEL_URL, VERCEL } = process.env;
+    let SITEURL
+    if (VERCEL) {
+        SITEURL = VERCEL_URL;
+    } else {
+        SITEURL = NODE_ENV !== 'production' ? DEV_URL : PROD_URL;
+    }
+
+    console.log(`VERCEL`, VERCEL);
+    console.log(`VERCEL_URL`, VERCEL_URL);
+    console.log(`NODE_ENV`, NODE_ENV);
+    console.log(`SITEURL api`, `${SITEURL}/api/tweet`);
+
+
     return (
         <Layout>
             <MainHeader title="Explore"></MainHeader>
